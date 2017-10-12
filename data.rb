83e4ec54181caf4639fc23a3d2fb4dc5c4d01c58
fall <xiaoyuner2014@gmail.com>
2017-10-11 23:09:00 -0400

    fds

--- data.rb
+++ data.rb
@@ -1,9 +1,12 @@
 
-# @hots = [{:title => "Scarface Split Tee", :cat => 't-shirts'},
-# 	{:title => "Scarface Blimp Tee", :cat => 't-shirts'},
-# 	{:title => "The World Is Yours", :cat => 'sweatshirts'},
-# 	{:title => "The World Is Yours Desk", :cat => 'accessories'},
-# 	{:title => "Scarface Split", :cat => 'skate'}]
+@hots = [{:title => "Scarface Split Tee", :cat => 't-shirts'},
+	{:title => "Scarface Blimp Tee", :cat => 't-shirts'},
+	{:title => "Scarface Friend", :cat => 't-shirts'},
+	{:title => "Scarface Shower Tee", :cat => 't-shirts'},
+	{:title => "The World Is Yours", :cat => 'sweatshirts'},
+	{:title => "The World Is Yours Desk", :cat => 'accessories'},
+	{:title => "Scarface Split", :cat => 'skate'}]
 
-@hots = [{:title => "Wool Overcoat", :cat => 'jackets'},
-		{:title => "Truck", :cat => 'skate'}]
+# @hots = [{:title => "Wool Overcoat", :cat => 'jackets'},
+# 		{:title => "Shop", :cat => 'jackets'},
+# 		{:title => "Leopard", :cat => 'jackets'}]
